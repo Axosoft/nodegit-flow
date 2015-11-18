6cfab02f1cdec6cd4055d9b663c642b4cd607447
Jose Garcia <joseg@axosoft.com>
2015-11-17 18:16:38 -0700

    add nodegit.Flow.isInitialized()

--- src/init.js
+++ src/init.js
@@ -1,6 +1,27 @@
 function init(options) {
 }
 
+function isInitialized(repository) {
+  if (!repository) {
+    throw new Error('A repository is required.');
+  }
+
+  return repository.config()
+    .then(function(config) {
+      return Promise.all([
+        config.getString('gitflow.branch.master'),
+        config.getString('gitflow.branch.develop')
+      ])
+        .then(function() {
+          return true;
+        })
+        .catch(function() {
+          return false;
+        });
+    });
+}
+
 module.exports = {
-  init: init
+  init: init,
+  isInitialized: isInitialized
 };
