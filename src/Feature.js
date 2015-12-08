457abb1f1556f3db986ae79d3844b492da42244b
Kyle Smith <kyles@axosoft.com>
2015-12-08 15:01:27 -0700

    Added 'startFeature' method

--- src/Feature.js
+++ src/Feature.js
@@ -1,4 +1,6 @@
-var Config = require('./Config');
+const Promise = require('nodegit-promise');
+const NodeGit = require('nodegit');
+const Config = require('./Config');
 
 class Feature {
   constructor(repo, config) {
@@ -11,7 +13,7 @@ class Feature {
    * @param {Object} the repo to start a feature in
    * @param {String} new branch name to start feature with
    */
-  startFeature(repo, featureName) {
+  static startFeature(repo, featureName) {
     if (!repo) {
       return Promise.reject(new Error('Repo is required'));
     }
@@ -20,17 +22,21 @@ class Feature {
       return Promise.reject(new Error('Feature name is required'));
     }
 
-    var featurePrefix;
+    let featureBranchName;
     return Config.getConfig(repo)
-      .then(function(config) {
-        featurePrefix = config.featurePrefix;
-        var branchName = featurePrefix + featureName;
-        return repo.createBranch(branchName);
-      });
-  }
+      .then((config) => {
+        const featurePrefix = config['gitflow.prefix.feature'];
+        const developBranchName = config['gitflow.branch.develop'];
 
-  startFeatureOnCommit() {
-    // TODO
+        featureBranchName = featurePrefix + featureName;
+        return NodeGit.Branch.lookup(
+          repo,
+          developBranchName,
+          NodeGit.Branch.BRANCH.LOCAL
+        );
+      })
+      .then((developBranch) => NodeGit.Commit.lookup(repo, developBranch.target()))
+      .then((localDevelopCommit) => repo.createBranch(featureBranchName, localDevelopCommit));
   }
 
   /**
@@ -38,7 +44,7 @@ class Feature {
    * @param {Object} the repo to start a feature in
    * @param {String} branch name to finish feature with
    */
-  finishFeature() {
+  static finishFeature() {
     // TODO
   }
 
@@ -46,15 +52,15 @@ class Feature {
    * Instance method to start a feature
    * @param {String} branch name to finish feature with
    */
-  static startFeature() {
-    // TODO
+  startFeature(featureName) {
+    return Feature.startFeature(this.repo, featureName);
   }
 
   /**
    * Instance method to finish a feature
    * @param {String} branch name to finish feature with
    */
-  static finishFeature() {
+  finishFeature() {
     // TODO
   }
 }
