91e3565336fe2c8d3d89066b7158b496781e5841
Kyle Smith <kyles@axosoft.com>
2015-12-10 17:24:00 -0700

    Added .finishRelease

--- src/Release.js
+++ src/Release.js
@@ -52,8 +52,113 @@ class Release {
    * @param {Object} the repo to start a release in
    * @param {String} branch name to finish release with
    */
-  static finishRelease() {
-    // TODO
+  static finishRelease(repo, releaseName) {
+    if (!repo) {
+      return Promise.reject(new Error('Repo is required'));
+    }
+
+    if (!releaseName) {
+      return Promise.reject(new Error('Release name is required'));
+    }
+
+    let developBranch;
+    let releaseBranch;
+    let masterBranch;
+    let developCommit;
+    let releaseCommit;
+    let masterCommit;
+    let mergeCommit;
+    let versionPrefix;
+    return Config.getConfig(repo)
+      .then((config) => {
+        const developBranchName = config['gitflow.branch.develop'];
+        const releaseBranchName = config['gitflow.prefix.release'] + releaseName;
+        const masterBranchName = config['gitflow.branch.master'];
+        versionPrefix = config['gitflow.prefix.versiontag'];
+
+        // Get the develop, master, and release branch
+        return Promise.all(
+          [developBranchName, releaseBranchName, masterBranchName]
+            .map((branchName) => NodeGit.Branch.lookup(repo, branchName, NodeGit.Branch.BRANCH.LOCAL))
+        );
+      })
+      .then((branches) => {
+        developBranch = branches[0];
+        releaseBranch = branches[1];
+        masterBranch = branches[2];
+
+        // Get the commits that the develop, master, and release branches point to
+        return Promise.all(branches.map((branch) => repo.getCommit(branch.target())));
+      })
+      .then((commits) => {
+        developCommit = commits[0];
+        releaseCommit = commits[1];
+        masterCommit = commits[2];
+
+        // Merge the release branch into master
+        return NodeGit.Merge.commits(repo, releaseCommit, masterCommit);
+      })
+      .then((index) => {
+        if (!index.hasConflicts()) {
+          index.write();
+
+          // Write the merge index to the repo
+          return index.writeTreeTo(repo);
+        }
+
+        // Reject with the index if there are conflicts
+        return Promise.reject(index);
+      })
+      .then((oid) => {
+        const ourSignature = repo.defaultSignature();
+        const commitMessage = `Merged branch ${releaseBranch.name()} into ${masterBranch.name()}`;
+
+        // Create the merge commit of release into master
+        return repo.createCommit(
+          masterBranch.name(),
+          ourSignature,
+          ourSignature,
+          commitMessage,
+          oid,
+          [masterCommit, releaseCommit]
+        );
+      })
+      .then((oid) => NodeGit.Commit.lookup(repo, oid))
+      // Tag the merge commit on master
+      .then((commit) => {
+        const tagName = versionPrefix + releaseName;
+        const ourSignature = repo.defaultSignature();
+        return NodeGit.Tag.create(repo, tagName, commit, ourSignature, '', 0);
+      })
+      // Merge release into develop
+      .then(() => NodeGit.Merge.commits(repo, releaseCommit, developCommit))
+      .then((index) => {
+        if (!index.hasConflicts()) {
+          index.write();
+          return index.writeTreeTo(repo);
+        }
+
+        // Reject with the index if there are conflicts
+        return Promise.reject(index);
+      })
+      .then((oid) => {
+        const ourSignature = repo.defaultSignature();
+        const commitMessage = `Merged branch ${releaseBranch.name()} into ${developBranch.name()}`;
+        return repo.createCommit(
+          developBranch.name(),
+          ourSignature,
+          ourSignature,
+          commitMessage,
+          oid,
+          [developCommit, releaseCommit]
+        );
+      })
+      .then((_mergeCommit) => {
+        mergeCommit = _mergeCommit;
+        return repo.checkoutBranch(developBranch);
+      })
+      .then(() => releaseBranch.delete())
+      .then(() => mergeCommit);
   }
 
   /**
