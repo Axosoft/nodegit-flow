dac0df3a54d48ff4c6aae011f154f99ad1dc9a44
Kyle Smith <kyles@axosoft.com>
2015-12-18 16:22:03 -0700

    Added merge function to repo utils

--- src/utils/RepoUtils.js
+++ src/utils/RepoUtils.js
@@ -1,6 +1,18 @@
 const Promise = require('nodegit-promise');
+const NodeGit = require('nodegit');
 
 const RepoUtils = {
+  merge(toBranch, fromBranch, repo) {
+    return repo.mergeBranches(toBranch.name(), fromBranch.name(), null, NodeGit.Merge.PREFERENCE.NO_FASTFORWARD)
+      .then((result) => {
+        if (result.hasConflicts && result.hasConflicts()) {
+          return Promise.reject(result);
+        }
+
+        return Promise.resolve(result);
+      });
+  },
+
   rebase(toBranch, fromBranch, repo) {
     return repo.rebaseBranches(fromBranch.name(), toBranch.name(), undefined, undefined)
       .then((result) => {
