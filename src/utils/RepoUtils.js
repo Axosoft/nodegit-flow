ebf0c009ebf6cd75786cb466f2064792a18d0239
Kyle Smith <kyles@axosoft.com>
2015-12-21 11:20:08 -0700

    Improved merge function in utils

--- src/utils/RepoUtils.js
+++ src/utils/RepoUtils.js
@@ -1,15 +1,51 @@
 const Promise = require('nodegit-promise');
 const NodeGit = require('nodegit');
 
+const MergeUtils = require('./MergeUtils');
+
 const RepoUtils = {
   merge(toBranch, fromBranch, repo) {
-    return repo.mergeBranches(toBranch.name(), fromBranch.name(), null, NodeGit.Merge.PREFERENCE.NO_FASTFORWARD)
-      .then((result) => {
-        if (result.hasConflicts && result.hasConflicts()) {
-          return Promise.reject(result);
+    let treeOid;
+    return Promise.resolve()
+      .then(() => {
+        if (!toBranch.isHead()) {
+          return repo.checkoutBranch(toBranch);
+        }
+        return Promise.resolve();
+      })
+      .then(() => NodeGit.AnnotatedCommit.fromRef(repo, fromBranch))
+      .then((fromCommit) => {
+        const checkoutOpts = {
+          checkoutStrategy: NodeGit.Checkout.STRATEGY.SAFE | NodeGit.Checkout.STRATEGY.RECREATE_MISSING
+        };
+        return NodeGit.Merge.merge(repo, fromCommit, null, checkoutOpts);
+      })
+      .then(() => repo.openIndex())
+      .then((index) => {
+        if (index.hasConflicts && index.hasConflicts()) {
+          return Promise.reject(index);
         }
 
-        return Promise.resolve(result);
+        return index.writeTree();
+      })
+      .then((oid) => {
+        treeOid = oid;
+
+        return Promise.all([
+          repo.getHeadCommit(),
+          repo.getBranchCommit('MERGE_HEAD')
+        ]);
+      })
+      .then((commits) => {
+        const signature = repo.defaultSignature();
+        const message = MergeUtils.getMergeMessage(toBranch, fromBranch);
+
+        return repo.createCommit('HEAD', signature, signature, message, treeOid, commits);
+      })
+      .then((commitId) => {
+        repo.stateCleanup();
+
+        return commitId;
       });
   },
 
