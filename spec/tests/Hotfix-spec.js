3c438957080b8b0b8397279f9ee79a7d68998ffc
Kyle Smith <kyles@axosoft.com>
2015-12-21 11:21:12 -0700

    Removed console.log

--- spec/tests/Hotfix-spec.js
+++ spec/tests/Hotfix-spec.js
@@ -122,11 +122,7 @@ describe('Hotfix', function() {
       })
       .then(() => Hotfix.finishHotfix(this.repo, hotfixName))
       .then(() => expectFinishHotfixSuccess.call(this, hotfixBranch, fullTagName))
-      .then(done)
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
+      .then(done);
   });
 
   it('should be able to finish hotfix using flow instance', function(done) {
