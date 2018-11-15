83ea9db1e3099c88c2e0bbee21e47dc96f1c066e
Tyler Wanek <tylerw@axosoft.com>
2018-11-15 13:55:02 -0800

    If we find the branch, don't try to create it right afterward

--- src/Base.js
+++ src/Base.js
@@ -87,9 +87,11 @@ module.exports = (NodeGit, { constants }, { Config, Feature, Hotfix, Release }) 
         .then(() => {
           // Create the `develop` branch if it does not already exist
           return NodeGit.Branch.lookup(repo, developBranchName, NodeGit.Branch.BRANCH.LOCAL)
-            .catch(() => repo.getBranchCommit(`refs/remotes/origin/${developBranchName}`))
-            .catch(() => repo.getBranchCommit(masterBranchName))
-            .then((commit) => repo.createBranch(developBranchName, commit.id()));
+            .catch(() =>
+              repo.getBranchCommit(`refs/remotes/origin/${developBranchName}`)
+                .catch(() => repo.getBranchCommit(masterBranchName))
+                .then((commit) => repo.createBranch(developBranchName, commit.id()))
+            );
         })
         .then(() => repo.config())
         .then((config) => {
